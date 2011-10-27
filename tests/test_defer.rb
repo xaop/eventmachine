fef6a20c3506751f7ebb020a5edfbc1fcf49c99f
Joeri Samson <joeri@xaop.com>
2011-10-27 14:13:27 +0200

    Run threaded tests in thread safe way
    Every so often the tests would hang at this test. The reason is obvious:
    n += 1 is not an atomic operation, so after doing n += 1 twenty times in
    parallel there is a chance that n ends up being smaller than 20. This
    leads to the tests hanging forever

--- tests/test_defer.rb
+++ tests/test_defer.rb
@@ -1,14 +1,17 @@
 require 'em_test_helper'
 
+require 'thread'
+
 class TestDefer < Test::Unit::TestCase
 
   def test_defers
     n = 0
     n_times = 20
+    mutex = Mutex.new
     EM.run {
       n_times.times {
-        work_proc = proc { n += 1 }
-        callback = proc { EM.stop if n == n_times }
+        work_proc = proc { mutex.synchronize { n += 1 } }
+        callback = proc { EM.stop if mutex.synchronize { n == n_times } }
         EM.defer work_proc, callback
       }
     }
